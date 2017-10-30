7bb361b5e73f8a5e86dae95ef017886cb0da9a74
Yun Xu <yunx@zillowgroup.com>
2017-10-29 21:45:02 -0700

    prototype for correlation

---
+++ anomaly_detection_models/correlators/naive.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def correlate_metrics(time_series_matrix, target_ts_index, correlation_level=0.9):
+	"""
+	Correlate target metrics among a bunch of metrics.
+
+    Args:
+        time_series_matrix: time_series metrics.
+		target_ts_index: index of target time series data.
+		correlation_level: correlation level.
+		
+        Perf issues ?
+	"""
+	pass
+
+
+def calulate_correlation_table(time_series_matrix):
+	"""
+	Calculate correlation table based on the time_series metrics.
+
+    Args:
+        time_series_matrix: time_series metrics
+	"""
+	pass
