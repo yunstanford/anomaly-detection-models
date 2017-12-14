039c3a828a7100dbc36117820435b9d0bda43a1e
Yun Xu <yunx@zillowgroup.com>
2017-12-13 21:29:00 -0800

    training workable

--- anomaly_detection_models/predictors/lstm.py
+++ anomaly_detection_models/predictors/lstm.py
@@ -24,9 +24,8 @@ def load_data(ts_series, seq_len, batch_size=1, ratio=0.9):
     test_set = ts_seqs[N, :]
     # Let's shuffle training set...
     np.random.shuffle(train_set)
-
-    x_train = train_set[:, :-1]
-    y_train = train_set[:, :-1]
+    x_train = train_set[:N, :-1]
+    y_train = train_set[:N, :-1]
 
     x_test = train_set[N:, :-1]
     y_test = train_set[N:, -1]
@@ -34,29 +33,28 @@ def load_data(ts_series, seq_len, batch_size=1, ratio=0.9):
     # reshap input to be [samples, time step, features]
     x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
     y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
-
     return (x_train, y_train, x_test, y_test)
 
 
-def build_model(layers, seq_len=1, batch_size=1, dropout=0.2, activation="linear", loss="mse", optimizer="rmsprop"):
+def build_model(neuron=10, seq_len=1, dropout=0.2, activation="linear", loss="mse", optimizer="rmsprop"):
     model = Sequential()
 
     # First Layer LSTM
     model.add(
-        LSTM(layers[0], batch_input_shape=(batch_size, seq_len, 1), stateful=True, return_sequences=True)
+        LSTM(neuron, input_shape=(seq_len, 1), return_sequences=True)
     )
     model.add(Dropout(dropout))
 
-    # Second Layer LSTM
+    # # Second Layer LSTM
     model.add(
-        LSTM(layers[1], batch_input_shape=(batch_size, seq_len, 1), stateful=True, return_sequences=False)
+        LSTM(neuron, input_shape=(seq_len, 1), return_sequences=True)
     )
     model.add(Dropout(dropout))
 
-    # Flatten
-    # model.add(Flatten())
+    # # Flatten
+    # # model.add(Flatten())
 
-    # Feeds to fully connected normal layer
+    # # Feeds to fully connected normal layer
     model.add(Dense(1, activation=activation))
 
     # Compile model
@@ -133,17 +131,22 @@ def main():
     vals, ts = loop.run_until_complete(
         get_time_series("graphite.del.zillow.local", 80, "sumSeries(zdc.metrics.production.pre.*.*.rum.school-schoolsearchpage.domready.desktop.turnstile.in)", "-7d")
     )
-    vals = normalize(vals.reshape(-1, 1))
+
+    # Normalize Data
+    # vals = normalize(vals.reshape(-1, 1))
     print(vals)
+
+    # load data
     X_train, y_train, X_test, y_test = load_data(vals, seq_len=seq_len, batch_size=batch_size)
 
     # Build and Compile Model
-    model = build_model([50, 50], batch_size=batch_size, seq_len=seq_len)
+    model = build_model(neuron=100, seq_len=seq_len)
+
     # Training
     model.fit(
         X_train,
         y_train,
-        batch_size=batch_size,
+        # batch_size=batch_size,
         epochs=epochs,
         validation_split=0.05
     )
